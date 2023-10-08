20c1b3c5f0d32114ea17d5164d4793f8852c2437
AWALUDDIN S <awaluddin.sampe@gmail.com>
2023-10-08 17:08:33 +0800

    tugas fundamental android awaluddin s

---
+++ app/src/main/java/tugas/android/fundamental/User.kt
@@ -0,0 +1,10 @@
+package tugas.android.fundamental
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+    val username:String,
+    val password:String
+):Parcelable
